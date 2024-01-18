969dc60034ad2f8da71bfe8981abaecc5358d886
Vusigae <Vusigae>
2024-01-18 15:45:41 +0000

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/ env bash
 #using puppet to make cahnges to our configuration file
 
-file { '/0x0B-ssh/2-ssh_config':
+file { 'alx-system_engineering-devops/0x0B-ssh/2-ssh_config':
 	ensure => present,
 
 content =>"

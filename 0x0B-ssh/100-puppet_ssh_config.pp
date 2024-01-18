d36762cff838a25963ab591605714b2673b1fa6a
Vusigae <Vusigae>
2024-01-18 15:53:10 +0000

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
-#!/usr/bin/ env bash
-#using puppet to make cahnges to our configuration file
+#!/usr/bin/env bash
+#Using Puppet to make changes to our configuration file
 
-file { 'alx-system_engineering-devops/0x0B-ssh/2-ssh_config':
-	ensure => present,
-
-content =>"
-
-	#SHH CLIENT CONFIG
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
+file { '2-ssh_config':
+  path    => '/alx-system_engineering-devops/0x0B-ssh',  # Specify the correct path
+  ensure  => present,
+  content => "
+    # SSH CLIENT CONFIG
+    Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  ",
 }
+
